d73bb4a6e99ded32b0ece3e953f63f50be14a6e9
EvanReinstein <edreins@gmail.com>
2018-09-27 15:55:20 -0700

    adds smooth scroll and google map API

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
 
+// Map
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 37.78, lng: -122.44},
+  zoom: 8,
+  mapTypeId: 'terrain'
+});
+
+
+// Smooth Scroll
+$('nav a').on('click', function (e) {
+  var targetHref = $(this).attr('href');
+
+  $('html, body').animate({
+    scrollTop: $(targetHref).offset().top}, 1000);
+
+    e.preventDefault();
+});
