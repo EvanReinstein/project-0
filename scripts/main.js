953e5068e00946a99503c90fbe24e29d3181358d
EvanReinstein <edreins@gmail.com>
2018-09-30 13:32:18 -0700

    adds project images and basic carousel

--- scripts/main.js
+++ scripts/main.js
@@ -37,15 +37,6 @@ function makeMapMarker () {
 makeMapMarker();
 
 // Smooth Scroll
-
-// $('header a').on('click', function (e) {
-//   var targetHref = $(this).attr('href');
-//
-//   $('html, body').animate({
-//     scrollTop: $(targetHref).offset().top}, 1000);
-//
-//     e.preventDefault();
-// });
 $('a').on('click', function (event) {
   if (this.hash !== '') {
     event.preventDefault();
@@ -107,3 +98,65 @@ function navBarHighlight (id) {
 }
 
 ['About', 'Projects', 'Skills', 'Quotes', 'Contact'].forEach(navBarHighlight);
+
+// carousel
+function moveToSelected(element) {
+  console.log(element);
+  if (element === "next") {
+    var selected = $(".selected").next();
+  } else if (element === "prev") {
+    var selected = $(".selected").prev();
+  } else {
+    var selected = element;
+  }
+
+  var next = $(selected).next();
+  var prev = $(selected).prev();
+  // var prevSecond = $(prev).prev();
+  // var nextSecond = $(next).next();
+
+  $(selected).removeClass().addClass("selected");
+
+  $(prev).removeClass().addClass("prev");
+  $(next).removeClass().addClass("next");
+
+  // $(nextSecond).removeClass().addClass("nextRightSecond");
+  // $(prevSecond).removeClass().addClass("prevLeftSecond");
+  //
+  // $(nextSecond).nextAll().removeClass().addClass('hideRight');
+  // $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+
+}
+
+// Key Events
+// $(document).keydown(function(e) {
+//     switch(e.which) {
+//         case 37: // left
+//         moveToSelected('prev');
+//         break;
+//
+//         case 39: // right
+//         moveToSelected('next');
+//         break;
+//
+//         default: return;
+//     }
+//     e.preventDefault();
+// });
+
+$('#carousel div').click(function() {
+  moveToSelected($(this));
+});
+
+$('#prev').click(function() {
+  moveToSelected('prev');
+});
+
+$('#next').click(function() {
+  moveToSelected('next');
+});
+
+
+$('#next').click(function() {
+  moveToSelected('next');
+});
